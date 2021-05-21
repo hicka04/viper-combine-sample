4a275214bc2e8e27f8f9b013a97ba4934d0d0a7b
hicka04 <hicka04@gmail.com>
2021-05-21 22:00:04 +0900

    add ArticleSearch View

--- ViperCombineSample/Modules/ArticleSearch/View/ArticleSearchViewController.swift
+++ ViperCombineSample/Modules/ArticleSearch/View/ArticleSearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ArticleSearchViewController.swift
 //  ViperCombineSample
 //
 //  Created by hicka04 on 2021/05/21.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ArticleSearchViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .blue
     }
-
-
 }
-
